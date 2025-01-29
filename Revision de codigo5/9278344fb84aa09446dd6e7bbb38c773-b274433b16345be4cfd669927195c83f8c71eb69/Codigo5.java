92d5624dc6aa885210095755fe4c3506a55146f1
ElsyA0 <chelcy000@gmail.com>
2025-01-29 17:30:22 -0600

    Update Codigo5.java
    Se realiza la revision del codigo.

--- Revision de codigo5/9278344fb84aa09446dd6e7bbb38c773-b274433b16345be4cfd669927195c83f8c71eb69/Codigo5.java
+++ Revision de codigo5/9278344fb84aa09446dd6e7bbb38c773-b274433b16345be4cfd669927195c83f8c71eb69/Codigo5.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
 public class Codigo5 {
-
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
+//faltaba importar el scanner.
+	//falta iniciar la clase main
+	public class static void(Stirng[] args){
+//Los nombres de todas las variable deben ser mas claras y que hagan referencias obvias.
+	    Scanner s = new Scanner(System.in);//Faltaba colocar el system.in
+	    System.out.print("Introduzca un número: "); //estaba mal, tiene que teminar con comillas dobles o cambiarla por comillas simples
+	    int ni = s.nextLine(); //Si se deseaba guardar un numero entonces se debe colocar int en vez de String
+	    int c = ni; // si se desea cambiar el valor de un string a un int entonces esto esta mal. pero debido a la linea anterior se decide que se requiere cambiar por un int.
 	    
 	    int afo = 0;
 	    int noAfo = 0;
 	    
 	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
+		  int digito = (double)(ni % 10); //deberia ser un doble para que funcione. el cambio de tipo de dato si no entonces no tiene caso colocar (int) debido a que ya lo es.
 	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
 			afo++;
 	      } else {
@@ -27,3 +31,4 @@ public class Codigo5 {
 	  }
 	  
 	}
+}
